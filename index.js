29d601afcf3dd788cf8ff0309de1519a243dd5e7
iykesworld <kennethiyke12@gmail.com>
2025-02-22 22:02:48 +0000

    updated cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
     cors({
-      origin: ["http://localhost:5174","https://product-items-dashboard.onrender.com/", "https://products-required.onrender.com", "http://localhost:5173"],
+      origin: ["http://localhost:5174","https://product-items-dashboard.onrender.com", "https://products-required.onrender.com", "http://localhost:5173"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
       credentials: true,
     })
